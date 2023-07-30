ebc79fcbdf69ef2c0220794649d58d8e3b1b7960
Ozlemhezer <118294663+Ozlemhezer@users.noreply.github.com>
2023-07-31 00:14:39 +0300

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<math.h>
 #include<stdlib.h>
-double f(int A[],int islem, int size)//hem diziyi hem de iþlem türünü belirten integer alacak.
+double f(int A[],int islem, int size)//hem diziyi hem de iÃ¾lem tÃ¼rÃ¼nÃ¼ belirten integer alacak.
 {
 	int i;
 	double carpim=1.0, toplam=0.0, h=0.0;
@@ -22,7 +22,7 @@ int main()
 {
 	int i;
 	double result;
-	int A[3]={6,3,1000};
+	int A[3]={6,3,10};
 	printf("islem giriniz\n1)aritmatik ortalama\n2)geometrik ortalama\n3)harmonik ortalama\n");
 	scanf("%d",&i);
 	result=f(A,i,3);
